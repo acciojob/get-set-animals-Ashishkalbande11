82da02788aace31e7b8c43c6860506d4ce007922
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-08-25 21:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 class Animal {
-	construtor(species){
-		this.species = species;
-	}
+    constructor(species) {
+        this.species = species;
+    }
 
-	makeSound(){
-		console.log("Some generic animal sound");
-	}
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
 }
 
 class Dog extends Animal {
-	bark(){
-		console.log("woof")
-	}
-    makeSound(){
-		console.log("The Siamese makes a sound");
-	}
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log("woof");
+    }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
-    makeSound(){
-		console.log("The Golden Retriever makes a sound");
-	}
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log("purr");
+    }
 }
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
