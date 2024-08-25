471f4be411ebfe200c0e012cf884decc1ddde1e1
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-08-25 20:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//complete this code
 class Animal {
 	construtor(species){
 		this.species = species;
@@ -13,14 +12,26 @@ class Dog extends Animal {
 	bark(){
 		console.log("woof")
 	}
+    makeSound(){
+		console.log("The Siamese makes a sound");
+	}
 }
 
 class Cat extends Animal {
 	purr(){
 		console.log("purr");
 	}
+    makeSound(){
+		console.log("The Golden Retriever makes a sound");
+	}
 }
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
 
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
